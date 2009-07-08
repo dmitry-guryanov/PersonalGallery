213f046097b4586d33f1e266e47899e9c1135ee5
Dmitry Guryanov <guryanov@dgap.mipt.ru>
2009-07-08 21:56:22 +0400

    add some extra imports to helpers because of moving to pylons 0.9.7

--- gallery/lib/helpers.py
+++ gallery/lib/helpers.py
@@ -4,3 +4,7 @@ Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to both as 'h'.
 """
 from webhelpers import *
+# from webhelpers.html.tags import checkbox, password 
+from routes import url_for
+from routes import redirect_to
+from webhelpers.rails.wrapped import * 
