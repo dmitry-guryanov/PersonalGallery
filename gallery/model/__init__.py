21db6aa186e73c0066683337f8c6cd7adc3cf03a
Guryanov Dmitry <dimak@dgap.mipt.ru>
2010-01-22 03:24:13 +0300

    update model to new style from pytlons-0.9.7

--- gallery/model/__init__.py
+++ gallery/model/__init__.py
@@ -45,8 +45,6 @@ class Album(Base):
 def init_model(engine):
 	"""Call me before using any of the tables or classes in the model."""
 
-	sm = orm.sessionmaker(autoflush=True, transactional=True, expire_on_commit = True,  bind=engine)
-
+	meta.Session.configure(bind=engine)
 	meta.engine = engine
-	meta.Session = orm.scoped_session(sm)
 
--- gallery/model/meta.py
+++ gallery/model/meta.py
@@ -1,9 +1,12 @@
 """SQLAlchemy Metadata and Session object"""
 from sqlalchemy import MetaData
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+__all__ = ['Session', 'engine', 'metadata']
 
 engine = None
 
-Session = None
+Session = scoped_session(sessionmaker())
 
 metadata = MetaData()
 
