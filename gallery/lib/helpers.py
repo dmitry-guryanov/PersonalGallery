c651c603cc741fd28cb18f05a843b1fe80ca0541
root <root@guryanov.org>
2011-04-22 17:04:05 +0400

    import secure_form from proper place

--- gallery/lib/helpers.py
+++ gallery/lib/helpers.py
@@ -5,6 +5,7 @@ available to Controllers. This module is available to both as 'h'.
 """
 from webhelpers import *
 from webhelpers.html.tags import *
-from webhelpers.html.secure_form import *
+from webhelpers.pylonslib.secure_form import *
+
 from routes import redirect_to
 
