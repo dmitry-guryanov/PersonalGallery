35582633e4f85c74112bdc6273a65ccb93487276
Dmitry Guryanov <dguryanov@parallels.com>
2011-04-25 23:06:12 +0400

    add header.py

---
+++ gallery/lib/header.py
@@ -0,0 +1,8 @@
+from pylons import tmpl_context as c
+from gallery.model import meta, Photo, Album
+
+def setup_header():
+	s = meta.Session
+
+	# top albums
+	c.top_albums = s.query(Album).filter(Album.parent_id == 0).filter(Album.id != 0).all()
