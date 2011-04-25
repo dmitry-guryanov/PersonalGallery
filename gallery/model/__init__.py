7f2d9eec30996158c9f5317400a02248e05e68e9
Dmitry Guryanov <dguryanov@parallels.com>
2011-04-25 22:45:38 +0400

    cleanup model.__init__.py

--- gallery/model/__init__.py
+++ gallery/model/__init__.py
@@ -1,28 +1,16 @@
 import os
 import re
 
-from logging import info
-
 import sqlalchemy as sa
 from sqlalchemy import orm, Column, ForeignKey
 from sqlalchemy.orm import relation, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import *
 
-#from gallery.model import meta
 from gallery.model.meta import Session, Base
-#from meta import Base
 
 permanent_store = None
 web_static_path = None
-
-def resync():
-	"""Renews SQLAlchemy session with current thread"""
-	del sac.session_current.current
-
-def flush():
-	"""Flushes all changes to database"""
-	sac.session.flush()
 
 class Photo(Base):
 	__tablename__ = "photos"
@@ -37,12 +25,6 @@ class Photo(Base):
 	hidden = Column(Boolean)
 
 	def get_path(self):
-		info("PHOTO")
-		info("self: %s" % self)
-		info("album_id: %s" % str(self.album_id))
-		info("name: %s" % self.name)
-		info("permanent_store: %s" % permanent_store)
-
 		return os.path.join(permanent_store,
 					str(self.album_id), self.name)
 
@@ -90,11 +72,6 @@ def init_model(engine, config):
 
 	Session.configure(bind=engine)
 
-#	sm = orm.sessionmaker(bind=engine)
-
-#	meta.engine = engine
-#	meta.Session = orm.scoped_session(sm)
-	print "config=%s" % str(config)
 	permanent_store = config.get('permanent_store')
 	web_static_path = config.get('web_static_path')
 
