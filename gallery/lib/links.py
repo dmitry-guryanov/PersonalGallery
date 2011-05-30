b1e88e7c74e6f3761933909fd8589d30e44b0cac
Dmitry Guryanov <dmitry.guryanov@gmail.com>
2011-05-30 05:04:08 +0400

    change link description format - change to name, <photo|album|article>, *args

--- gallery/lib/links.py
+++ gallery/lib/links.py
@@ -9,5 +9,5 @@ def get_link(req, s):
 	s = unicode(s, "utf-8")
 
 	items = map(lambda x: x.strip(), s.split(','))
-	url = req.resource_url(get_root()[items[0]], items[1])
-	return MyLink(items[2], url)
+	url = req.resource_url(get_root()[items[1]], *items[2:])
+	return MyLink(items[0], url)
